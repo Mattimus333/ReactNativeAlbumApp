cdf8a0e101836c85fb3c7d8d2d21fd16a089d9f8
Matt <mmuhr88@gmail.com>
2018-09-11 12:20:13 -0700

    started creating a albumList component

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 //create a component
 
-const App = () => (
-  <Header headerText={'Albums'} />
-);
+const App = () => {
+  return (
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+  )
+};
 
 //render it to the device
 AppRegistry.registerComponent('albums', () => App);
