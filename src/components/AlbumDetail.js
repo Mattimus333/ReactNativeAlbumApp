fbc035a3958936cc505665a0ea3e25b39891e2a9
Matt <mmuhr88@gmail.com>
2018-09-12 11:51:50 -0700

    user can scroll through albums and it looks gooood

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import { Text } from 'react-native';
-import Card from './Card'
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardItem from './CardItem';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerContentStyle,
+    headerTextStyle,
+    imageStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+  } = styles;
+
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardItem>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={thumbnailStyle}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardItem>
+      <CardItem>
+      <Image
+        source={{ uri: image }}
+        style={imageStyle}
+      />
+      </CardItem>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
 };
 
  export default AlbumDetail;
